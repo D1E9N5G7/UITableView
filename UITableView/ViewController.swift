0734158564ed23833034148ac3dbcbec9e9715dc
DENG <75176097@qq.com>
2019-04-28 23:25:46 +0800

    UITableView

--- UITableView/ViewController.swift
+++ UITableView/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let mUITableView = UITableView(frame: self.view.bounds)
+        mUITableView.delegate = self
+        mUITableView.dataSource = self
+        self.view.addSubview(mUITableView)
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let reuseCellId = "reuse"
+        var cell = tableView.dequeueReusableCell(withIdentifier: reuseCellId)
+        if(cell == nil){
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: reuseCellId)
+        }
+        cell?.textLabel?.text = "title"
+        cell?.detailTextLabel?.text = "description on title"
+        return cell!
+    }
+    
 
 }
 
